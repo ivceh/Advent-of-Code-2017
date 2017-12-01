81271ce4757a7c850e82cafb8527ed87baff2b92
ivceh <ivan.ceh1234@gmail.com>
2017-12-01 20:57:23 +0100

    Day 1, part B solved

---
+++ Day01/AoC2017_1B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    int sum = 0;
+
+    cin >> s;
+
+    for (int i=0; i<s.size(); ++i)
+    {
+        if (i < s.size()/2) // if it is the last index in s
+        {
+            if (s[i] == s[i+s.size()/2])
+                sum += s[i] - '0'; // get number value for digit from ASCII
+        }
+        else
+        {
+            if (s[i] == s[i-s.size()/2])
+                sum += s[i] - '0'; // get number value for digit from ASCII
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
